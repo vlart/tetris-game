aa9a8027dccfe48129e387983072fc7cfc070f14
Artiom Cavtea <artiomcav@gmail.com>
2026-08-01 19:30:30 +0300

    fix themes and timing

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const cacheName = "tetris-game-v27";
+const cacheName = "tetris-game-v34";
 const filesToCache = [
   "./",
   "./index.html",
-  "./style.css?v=27",
-  "./game.js?v=27",
+  "./style.css?v=34",
+  "./game.js?v=34",
   "./manifest.json",
   "./icon.svg",
   "./tetris-logo.png"
